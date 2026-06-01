5a8379af1ae56a29aa17630e075f29423b0dff26
文宇浩 <2023010832@secoder.net>
2026-06-01 21:38:16 +0800

    stacking package

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,7 @@
 from src.models.gmm_map import GMMMAPClassifier, gmm_cfg
 from src.models.gbdt import GBDTClassifier, gbdt_cfg, params_from_cfg as gbdt_params_from_cfg
 from src.models.kernel_svm import KernelSVMClassifier, svm_cfg
+from src.models.stacking import StackingClassifier, stacking_cfg
 
 __all__ = [
     "GMMMAPClassifier",
@@ -10,4 +11,6 @@ __all__ = [
     "GBDTClassifier",
     "gbdt_cfg",
     "gbdt_params_from_cfg",
+    "StackingClassifier",
+    "stacking_cfg",
 ]
